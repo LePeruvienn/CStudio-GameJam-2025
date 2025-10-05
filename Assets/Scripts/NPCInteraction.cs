ed8ad51d25f4ae8326310b0f24be2ed6c210e444
LePeruvienn <koto91@free.fr>
2025-10-05 12:00:52 +0200

    Added NPC kill

--- Assets/Scripts/NPCInteraction.cs
+++ Assets/Scripts/NPCInteraction.cs
@@ -13,9 +13,24 @@ public class NPCInteraction : MonoBehaviour
 	[SerializeField] private UnityEvent actionIfYes;
 	[SerializeField] private UnityEvent actionIfNo;
 
+	[Header("Die Animation")]
+	[SerializeField] private float deathJumpForce = 6f;
+	[SerializeField] private float deathRotateSpeed = 2f;
+	[SerializeField] private float deathDuration = 10f;
+
+	private Rigidbody2D _rigidbody;
+	private Collider2D _collider;
+
+	private bool _isDying = false;
+	private float _deathElapsedTime = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
+		// Get NPCs Components
+		_rigidbody = GetComponent<Rigidbody2D>();
+		_collider = GetComponent<Collider2D>();
+
 		// FOR DEBUG !! TO DELETE AFTER
 		// TODO MAKE ALL SCRIPTABLES OBJECTS RESET WHEN NEW GAME
 		npcData.isDead = false;
@@ -25,6 +40,21 @@ public class NPCInteraction : MonoBehaviour
         if (npcData.isDead)
 			Destroy(gameObject);
     }
+
+	private void Update()
+	{
+		// Return if player is not falling
+		if (!_isDying || _rigidbody.velocity.y > 0) return;
+
+		if (_deathElapsedTime > deathDuration)
+			Destroy(gameObject);
+
+		_deathElapsedTime += Time.deltaTime;
+
+		// Make player rotate over time
+		float z = deathRotateSpeed * Time.deltaTime * 1000;
+		transform.Rotate (new Vector3(0, 0, z));
+	}
 
 	public void Interact()
 	{
@@ -39,6 +69,23 @@ public class NPCInteraction : MonoBehaviour
 			npcData.dialogIndex++;
 	}
 
+	public void KillNPC()
+	{
+		npcData.isDead = true;
+		_isDying = true;
+
+		// Disable collider
+		_collider.enabled = false;
+
+		// Make player jump & reset x velocity
+		Vector2 velocity = _rigidbody.linearVelocity;
+		velocity.y = deathJumpForce;
+		velocity.x = 0;
+		_rigidbody.constraints = RigidbodyConstraints2D.None;
+		_rigidbody.linearVelocity = velocity;
+		_rigidbody.gravityScale = 1f;
+	}
+
 	public void GiveItem(string itemName) {
 		
 		playerData.AddItem(itemName);
@@ -49,10 +96,5 @@ public class NPCInteraction : MonoBehaviour
 		playerData.AddItem(itemName);
 		Destroy(gameObject);
 		npcData.isDead = true;
-	}
-
-	public void Die() {
-
-		Debug.Log("Au secours de meurt");
 	}
 }
